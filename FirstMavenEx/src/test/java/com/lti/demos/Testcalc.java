097684ede7f3ca101a25981c3e1da861eca9661c
NandhiniR012 <ravinandhini1999@gmail.com>
2022-03-15 23:22:22 +0530

    first commit

---
+++ FirstMavenEx/src/test/java/com/lti/demos/Testcalc.java
@@ -0,0 +1,13 @@
+package com.lti.demos;
+
+public class Testcalc {
+
+	public static void main(String[] args) {
+		
+				 
+		     Calculator c= new Calculator();
+		     int r=c.add(200 , 100);
+		     System.out.println(r);
+			}
+		}
+
